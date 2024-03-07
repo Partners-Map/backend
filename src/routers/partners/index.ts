d40bc74c6601590893f7bbb173f125b334c93bd6
Daniel <danielusovdev@gmail.com>
2024-03-07 10:20:25 +0300

    feat: partner's CRUD

--- src/routers/partners/index.ts
+++ src/routers/partners/index.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from 'fastify/types/instance';
-import { getAllPartners } from '../../services/partner';
+import PartnersService from '../../services/partner';
 
-export default async (fastify: FastifyInstance) => {
-  fastify.get('/', { schema: { tags: ['partners'] } }, async (req, rep) => {
-    rep.code(200).send(await getAllPartners(fastify));
-  });
+export default async (fastify: FastifyInstance): Promise<void> => {
+  fastify.get(
+    '/',
+    {
+      schema: { tags: ['partner'] }
+    },
+    async (req, rep) => {
+      rep.code(200).send(await PartnersService.getAll(fastify));
+    }
+  );
 };
